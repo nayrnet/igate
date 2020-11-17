52de05eeb5ac17248b0bc1c7d75fc972889387ae
Ryan <nayr@direwolf.nayr.net>
2020-11-16 20:03:46 -0700

    fix symlinks

--- direwolf/start-dw.sh
+++ direwolf/start-dw.sh
@@ -1,1 +1,2 @@
-/usr/local/bin/start-dw.sh
+#!/bin/bash
+/usr/local/bin/direwolf -T %H:%M:%S -a 0 -t 0 -c /etc/direwolf/direwolf.conf -L /var/log/direwolf/direwolf.log | frontail --url-path /direwolf --disable-usage-stats --ui-highlight-preset /etc/direwolf/direwolf.json -t dark --ui-highlight -n 60 -
